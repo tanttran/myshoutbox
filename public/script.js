8879f6fbd30f05f01e718d004c21a21ec162ead6
tantran <octttran@gmail.com>
2016-09-18 23:35:54 -0700

    added website layout using angularJS

--- public/script.js
+++ public/script.js
@@ -1,27 +1,13 @@
 var app = angular.module('myShoutBox', []);
 
-var app = angular.module('myShoutBox', ['ngRoute', 'ngCookies']);
 
-    app.config(function($routeProvider, $locationProvider) {
-      $routeProvider
-      .when('/', {
-        templateUrl: 'home.html',
-        controller: 'HomeController',
-      })
-      .when('/signup', {
-        templateUrl: 'signup.html',
-        controller: 'SignupController',
-      })
-      .when('/audittrail', {
-        templateUrl: 'audittrail.html',
-        controller: 'AuditTrailController',
-      })
-      .when('/404', {
-        templateUrl: '404.html'
-      })
-      .otherwise({
-        redirectTo: '/404'
-      })
 
-      $locationProvider.html5Mode(true);
-    });
+app.controller('HomeController', function ($scope) {
+  $scope.shout = [
+  'One',
+  'Two',
+  'Three',
+  'Four',
+  'Test'
+];
+});
