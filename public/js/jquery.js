8d618cba6f55ffd237944de5d826d08501845cb9
tantran <octttran@gmail.com>
2016-10-01 18:26:26 -0700

    added jquery

---
+++ public/js/jquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+  // $('.itemName').on('click','li', function(){
+  //   console.log('active');
+  //   $(this).addClass('active').siblings().removeClass('active');
+  // });
+
+  $('.shoutList').on('click','li', function(){
+    console.log('active');
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+
+
+});
